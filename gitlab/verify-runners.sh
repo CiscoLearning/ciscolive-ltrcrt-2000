bf3b0933ab44edc52a4eb10c5e23e41db050e282
Joe Clarke <jclarke@cisco.com>
2021-03-17 17:40:39 -0400

    Use the canonical URL locations.

--- gitlab/verify-runners.sh
+++ gitlab/verify-runners.sh
@@ -20,12 +20,12 @@ create_gitlab_token () {
       --data-urlencode "authenticity_token=${csrf_token}"
 
   # send curl GET request to personal access token page to get auth token
-  body_header=$(curl -L --silent -H 'user-agent: curl' -b /tmp/cookies.txt -i "${gitlab_host}/profile/personal_access_tokens" -s)
+  body_header=$(curl -L --silent -H 'user-agent: curl' -b /tmp/cookies.txt -i "${gitlab_host}/-/profile/personal_access_tokens" -s)
   csrf_token=$(echo $body_header | perl -ne 'print "$1\n" if /authenticity_token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
 
   # curl POST request to send the "generate personal access token form"
   # the response will be a redirect, so we have to follow using `-L`
-  body_header=$(curl --silent -L -b /tmp/cookies.txt "${gitlab_host}/profile/personal_access_tokens" \
+  body_header=$(curl --silent -L -b /tmp/cookies.txt "${gitlab_host}/-/profile/personal_access_tokens" \
       --data-urlencode "authenticity_token=${csrf_token}" \
       --data 'personal_access_token[name]=golab-generated&personal_access_token[expires_at]=&personal_access_token[scopes][]=api')
 
