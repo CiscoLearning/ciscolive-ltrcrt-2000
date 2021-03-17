c2113343c2517fdfe1d60f5597eb37766a7b4f54
Joe Clarke <jclarke@cisco.com>
2021-03-17 10:31:26 -0400

    Put .virlrc in the home dir.

--- init.sh
+++ init.sh
@@ -4,8 +4,12 @@ pip install pyats genie requests cmlutils
 init_common
 
 echo "Copying initial files"
-cp ${LAB}/init/virlrc ${LABDIR}/.virlrc
+cp ${LAB}/init/virlrc ${HOME}/.virlrc
 
+echo "Shutting down default CML lab"
+cml down -n "Multi Platform Network"
+
+# TODO: Import Production configuration
 
 echo "Spinning up GitLab-CE"
 cwd=$(pwd)
