e47f790f603edc8ca6887b23be11350220f7ad7c
Joe Clarke <jclarke@cisco.com>
2021-03-19 09:14:32 -0400

    More copies and suppress errors.

--- init.sh
+++ init.sh
@@ -5,18 +5,19 @@ init_common
 
 echo "Copying initial files"
 cp -f ${LAB}/init/virlrc ${HOME}/.virlrc
-cp -r ${LAB}/init/iac-infra ${LABDIR}
+cp -rf ${LAB}/init/iac-infra ${LABDIR}
+cp -f ${LAB}/init/vlan-fabric.yml ${LABDIR}
 
 echo "Shutting down default CML lab"
 cml down -n "Multi Platform Network"
 
 echo "Shutting down and deleting any previous labs"
-(cml use -n "Production" && cml rm -f --no-confirm) || return 0
-(cml use -n "Testing" && cml rm -f --no-confirm) || return 0
+(cml use -n "Production" && cml rm -f --no-confirm) >/dev/null 2>&1 || return 0
+(cml use -n "Testing" && cml rm -f --no-confirm) >/dev/null 2>&1 || return 0
 
 echo "Spinning up lab HOLOPS-2800 lab topologies"
-cml up -f ${LAB}/helper-files/Production.yaml
-cml up -f ${LAB}/helper-files/Testing.yaml
+cml up -f ${LAB}/helper-files/Production.yaml >/dev/null 2>&1
+cml up -f ${LAB}/helper-files/Testing.yaml >/dev/null 2>&1
 
 echo "Spinning up GitLab-CE"
 cwd=$(pwd)
