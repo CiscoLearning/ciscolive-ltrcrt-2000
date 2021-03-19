ac42f0819b96b4c06e7c3a82fe1f8d2ccec1fd7c
Joe Clarke <jclarke@cisco.com>
2021-03-19 09:00:07 -0400

    Remove any existing lab instances.

--- init.sh
+++ init.sh
@@ -5,9 +5,14 @@ init_common
 
 echo "Copying initial files"
 cp -f ${LAB}/init/virlrc ${HOME}/.virlrc
+cp -r ${LAB}/init/iac-infra ${LABDIR}
 
 echo "Shutting down default CML lab"
 cml down -n "Multi Platform Network"
+
+echo "Shutting down and deleting any previous labs"
+(cml use -n "Production" && cml rm -f --no-confirm) || return 0
+(cml use -n "Testing" && cml rm -f --no-confirm) || return 0
 
 echo "Spinning up lab HOLOPS-2800 lab topologies"
 cml up -f ${LAB}/helper-files/Production.yaml
