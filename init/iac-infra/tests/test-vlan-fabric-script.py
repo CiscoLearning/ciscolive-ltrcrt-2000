7498acbb1d8b95326e0218ecd7c3e4e619f9cadb
Joe Clarke <jclarke@cisco.com>
2021-03-20 11:24:35 -0400

    Add some fixes and debugging.
    Also pet flak8 while at it.

--- init/iac-infra/tests/test-vlan-fabric-script.py
+++ init/iac-infra/tests/test-vlan-fabric-script.py
@@ -14,16 +14,15 @@ except ImportError:
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
-# From https://stackoverflow.com/questions/18759512/expand-a-range-which-looks-like-1-3-6-8-10-to-1-2-3-6-8-9-10
-def expand_range(r):
-    l = [
-        s.split("-") for s in r.split(",")
-    ]  # Extract each comma-separated range element
-    l = [
-        range(int(i[0]), int(i[1]) + 1) if len(i) == 2 else i for i in l
-    ]  # expand the ranges
+IGNORE_VLANS = ["1", "1002", "1003", "1004", "1005"]
 
-    return [int(item) for sl in l for item in sl]
+
+def expand_range(r):
+    # From https://stackoverflow.com/questions/18759512/expand-a-range-which-looks-like-1-3-6-8-10-to-1-2-3-6-8-9-10
+    rl = [s.split("-") for s in r.split(",")]  # Extract each comma-separated range element
+    rl = [range(int(i[0]), int(i[1]) + 1) if len(i) == 2 else i for i in rl]  # expand the ranges
+
+    return [int(item) for sl in rl for item in sl]
 
 
 class VlanSetup(aetest.CommonSetup):
@@ -31,14 +30,8 @@ class VlanSetup(aetest.CommonSetup):
     def connect_to_devices(self):
         creds = {}
         vfabric = {}
-        cred_file = os.path.realpath(
-            os.path.join(
-                os.path.dirname(__file__), "..", "ansible", "group_vars", "all.yml"
-            )
-        )
-        fabric_file = os.path.realpath(
-            os.path.join(os.path.dirname(__file__), "..", "..", "vlan-fabric.yml")
-        )
+        cred_file = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "ansible", "group_vars", "all.yml"))
+        fabric_file = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "..", "vlan-fabric.yml"))
         with open(cred_file) as fd:
             creds = load(fd, Loader=Loader)
 
@@ -49,11 +42,7 @@ class VlanSetup(aetest.CommonSetup):
         os.environ["PYATS_PASSWORD"] = creds["ansible_ssh_pass"]
         os.environ["PYATS_AUTH_PASS"] = creds["ansible_ssh_pass"]
 
-        testbed = tbload(
-            os.path.realpath(
-                os.path.join(os.path.dirname(__file__), "testbed-testing.yml")
-            )
-        )
+        testbed = tbload(os.path.realpath(os.path.join(os.path.dirname(__file__), "testbed-testing.yml")))
         self.parent.parameters["testbed"] = testbed
         self.parent.parameters["vfabric"] = vfabric["fabric"]
 
@@ -62,9 +51,7 @@ class VlanSetup(aetest.CommonSetup):
 
     @aetest.subsection
     def prepare_testcases(self, testbed):
-        aetest.loop.mark(
-            DistVlanCheck, device=[d.name for d in testbed if d.type == "dist-switch"]
-        )
+        aetest.loop.mark(DistVlanCheck, device=[d.name for d in testbed if d.type == "dist-switch"])
         # aetest.loop.mark(
         #    AccessVlanCheck,
         #    device=[d.name for d in testbed if d.type == "access-switch"],
@@ -76,9 +63,7 @@ class DistVlanCheck(aetest.Testcase):
     def setup(self, device, testbed):
         d = testbed.devices[device]
         if not d.connected:
-            self.failed(
-                f"Device {device} is not connected; failed to learn operational details"
-            )
+            self.failed(f"Device {device} is not connected; failed to learn operational details")
             return
 
         log.info(banner(f"Gathering VLAN info from {device}"))
@@ -90,10 +75,10 @@ class DistVlanCheck(aetest.Testcase):
 
     @aetest.test
     def vlan_exists_test(self, device, vfabric):
+        global IGNORE_VLANS
+
         has_failed = False
         table_data = []
-
-        IGNORE_VLANS = ["1", "1002", "1003", "1004", "1005"]
 
         vlans = [str(d["vlan_id"]) for d in vfabric["vlans"]["l2"]]
 
@@ -170,15 +155,17 @@ class DistVlanCheck(aetest.Testcase):
 
     @aetest.test
     def stp_check_ports(self, device, vfabric):
+        global IGNORE_VLANS
+
         has_failed = False
         table_data = []
         trunk_ports = [d["port"] for d in vfabric["trunk_ports"]["distribution"]]
 
-        IGNORE_VLANS = ["1", "1002", "1003", "1004", "1005"]
-
         for v, vinfo in self.stp_det["pvst"]["vlans"].items():
             if v in IGNORE_VLANS:
                 continue
+
+            log.info(f"XXX: VLAN ID is '{v}'")
 
             i = 0
             for port in trunk_ports:
@@ -186,9 +173,7 @@ class DistVlanCheck(aetest.Testcase):
                 table_row.append(device)
                 table_row.append(v)
                 table_row.append(port)
-                allowed_vlans = expand_range(
-                    str(vfabric["trunk_ports"]["distribution"][i]["allowed_vlans"])
-                )
+                allowed_vlans = expand_range(str(vfabric["trunk_ports"]["distribution"][i]["allowed_vlans"]))
                 if int(v) in allowed_vlans:
                     table_row.append("Y")
                     if port not in vinfo["interfaces"]:
