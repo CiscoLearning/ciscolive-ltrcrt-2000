6d684e729306c23e2a3a75a79ebffa0e8a2ab8f3
Joe Clarke <jclarke@cisco.com>
2022-06-16 12:09:12 -0400

    Add support for SSH keys.

--- gitlab/setup.sh
+++ gitlab/setup.sh
@@ -4,6 +4,7 @@ gitlab_host="http://198.18.1.11"
 gitlab_user="root"
 gitlab_password="C1sco12345"
 gitlab_wait_time=45
+devbox_user="developer"
 # prints colored text
 success () {
     COLOR="92m"; # green
@@ -25,9 +26,30 @@ until $(curl --output /dev/null --silent --head --fail ${gitlab_host}); do
 done
 success
 
+printf "Generating SSH key for ${gitlab_user}..."
+mkdir -p ~/${devbox_user}/.ssh
+output=$(/usr/bin/ssh-keygen -b 4096 -N "" -t rsa -f ~/${devbox_user}/.ssh/id_rsa 2>&1)
+chown -R ${devbox_user} ~/${devbox_user}/.ssh
+chmod 0700 ~/${devbox_user}/.ssh
+key=$(cat ~/${devbox_user}/.ssh/id_rsa.pub)
+if [ $? != 0 ]; then
+    echo "FAIL!"
+    echo "Result = ${output}"
+else
+    success
+fi
+
 printf "Setting default branch name..."
 token=$(curl -s -X POST -H "Content-type: application/json" --data-raw "{\"grant_type\": \"password\", \"username\": \"${gitlab_user}\", \"password\": \"${gitlab_password}\"}" ${gitlab_host}/oauth/token | jq -r .access_token)
 output=$(curl -s -X PUT -H "Authorization: Bearer ${token}" -H "Content-type: application/json" --data-raw "{\"default_branch_name\": \"main\"}" ${gitlab_host}/api/v4/application/settings)
+if [ $? != 0 ]; then
+    echo "FAIL!"
+    echo "Result = ${output}"
+else
+    success
+fi
+
+output=$(curl -s -X POST -H "Authorization: Bearer ${token}" -H "Content-type: application/json" --data-raw "{\"name\": \"Devbox\", \"key\": \"${key}\"}" ${gitlab_host}/api/v4/user/keys)
 if [ $? != 0 ]; then
     echo "FAIL!"
     echo "Result = ${output}"
